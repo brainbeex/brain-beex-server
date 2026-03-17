50101abd034ce1a18710d89485e872b1fde14796
Mohd Imran Hossain <hossainmohdimran@gmail.com>
2026-03-17 21:55:12 +0600

    firebase correction

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -9,11 +9,18 @@
 // export default admin;
 
 import admin from "firebase-admin";
-import fs from "fs";
+// import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("./src/config/firebaseServiceKey.json", "utf8")
-);
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync("./src/config/firebaseServiceKey.json", "utf8")
+// );
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
