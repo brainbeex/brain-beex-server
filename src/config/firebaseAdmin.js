d8841ff949d4e4ffaabac179d3370cbd0325be19
Mohd Imran Hossain <hossainmohdimran@gmail.com>
2026-03-24 21:18:19 +0600

    ready for vercel

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -20,35 +20,4 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-// console.log("PRIVATE KEY:", process.env.FIREBASE_PRIVATE_KEY);
-// console.log("ENV TEST:", process.env.FIREBASE_PROJECT_ID);
-
 export default admin;
-
-
-
-// import admin from "firebase-admin";
-// import serviceAccount from "./firebaseServiceKey.json" assert { type: "json" };
-
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-// export default admin;
-
-// import admin from "firebase-admin";
-// import fs from "fs";
-
-// const serviceAccount = JSON.parse(
-//   fs.readFileSync("./src/config/firebaseServiceKey.json", "utf8")
-// );
-
-// const serviceAccount = {
-//   type: "service_account",
-//   project_id: process.env.FIREBASE_PROJECT_ID,
-//   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-//   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-// };
-
-
