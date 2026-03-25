f10406ce32f3801c6f9709595f8387295c98d74a
Mohd Imran Hossain <hossainmohdimran@gmail.com>
2026-03-25 22:03:02 +0600

    firebaseAdmin.js updated

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,23 +1,50 @@
-import dotenv from "dotenv";
-dotenv.config(); 
-
 import admin from "firebase-admin";
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+let app;
 
-if (!privateKey) {
-  throw new Error("FIREBASE_PRIVATE_KEY is missing in environment variables");
+if (!admin.apps.length) {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+  if (!privateKey) {
+    console.error("❌ Missing FIREBASE_PRIVATE_KEY");
+  }
+
+  app = admin.initializeApp({
+    credential: admin.credential.cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: privateKey?.replace(/\\n/g, "\n"),
+    }),
+  });
 }
 
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: privateKey.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-};
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
 export default admin;
+
+
+
+
+
+
+// import dotenv from "dotenv";
+// dotenv.config(); 
+
+// import admin from "firebase-admin";
+
+// const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+// if (!privateKey) {
+//   throw new Error("FIREBASE_PRIVATE_KEY is missing in environment variables");
+// }
+
+// const serviceAccount = {
+//   type: "service_account",
+//   project_id: process.env.FIREBASE_PROJECT_ID,
+//   private_key: privateKey.replace(/\\n/g, "\n"),
+//   client_email: process.env.FIREBASE_CLIENT_EMAIL,
+// };
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// export default admin;
