066b10b3cb9ef3f1dbf6a573dfda1e9c83fe3a87
brainbeex <brainbeex@gmail.com>
2026-04-15 22:45:09 +0600

    fix firebase + debug

--- api/index.js
+++ api/index.js
@@ -1,18 +1,14 @@
-
 import app from "../src/app.js";
 import serverless from "serverless-http";
 import connectDB from "../src/config/db.js";
 
 const handler = async (req, res) => {
-  await connectDB(); // 🔥 VERY IMPORTANT
+  console.log("🔥 FUNCTION START");
+
+  await connectDB();
+  console.log("✅ DB CONNECTED");
+
   return serverless(app)(req, res);
 };
 
 export default handler;
-
-
-
-// import app from "../src/app.js";
-// import serverless from "serverless-http";
-
-// export default serverless(app);
--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,23 +1,25 @@
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 
 import admin from "firebase-admin";
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+if (!admin.apps.length) {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
 
-if (!privateKey) {
-  throw new Error("FIREBASE_PRIVATE_KEY is missing in environment variables");
+  if (!privateKey) {
+    throw new Error("FIREBASE_PRIVATE_KEY is missing");
+  }
+
+  const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: privateKey.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  };
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
-
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: privateKey.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-};
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
 
 export default admin;
