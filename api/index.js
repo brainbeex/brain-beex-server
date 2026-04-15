dd7b51a42f8bd12d58c58a39cbb5b2e41fa5f98f
brainbeex <brainbeex@gmail.com>
2026-04-15 22:26:15 +0600

    fix mongodb connection for vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
+
 import app from "../src/app.js";
 import serverless from "serverless-http";
+import connectDB from "../src/config/db.js";
 
-export default serverless(app);
+const handler = async (req, res) => {
+  await connectDB(); // 🔥 VERY IMPORTANT
+  return serverless(app)(req, res);
+};
+
+export default handler;
+
+
+
+// import app from "../src/app.js";
+// import serverless from "serverless-http";
+
+// export default serverless(app);
---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+    throw error;
+  }
+};
+
+export default connectDB;
