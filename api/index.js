4bf90bb6c459f325f288b80628718d6d6df2b360
brainbeex <brainbeex@gmail.com>
2026-04-18 00:54:33 +0600

    Temporary bypass express

--- api/index.js
+++ api/index.js
@@ -1,21 +1,30 @@
-import app from "../src/app.js";
-import serverless from "serverless-http";
-import connectDB from "../src/config/db.js";
+// import app from "../src/app.js";
+// import serverless from "serverless-http";
+// import connectDB from "../src/config/db.js";
 
-let isReady = false;
+// let isReady = false;
 
-const init = async () => {
-  if (!isReady) {
-    console.log("🔥 Initializing server...");
-    await connectDB();
-    console.log("✅ MongoDB connected");
-    isReady = true;
-  }
-};
+// const init = async () => {
+//   if (!isReady) {
+//     console.log("🔥 Initializing server...");
+//     await connectDB();
+//     console.log("✅ MongoDB connected");
+//     isReady = true;
+//   }
+// };
 
-const handler = serverless(app);
+// const handler = serverless(app);
 
-export default async (req, res) => {
-  await init(); // connect ONLY once
-  return handler(req, res); // ❌ no await here
-};
+// export default async (req, res) => {
+//   await init(); // connect ONLY once
+//   return handler(req, res); // ❌ no await here
+// };
+
+
+export default function handler(req, res) {
+  console.log("🔥 DIRECT FUNCTION HIT");
+
+  res.status(200).json({
+    status: "DIRECT_OK",
+  });
+}
