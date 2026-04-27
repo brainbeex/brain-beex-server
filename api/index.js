adb0539ffe59b6de4fcba60c97c750d7caaaff9f
brainbeex <brainbeex@gmail.com>
2026-04-27 22:54:10 +0600

    access fix again 2

--- api/index.js
+++ api/index.js
@@ -47,7 +47,6 @@ import connectDB from "../src/config/db.js";
 
 export default async function handler(req, res) {
   // ✅ FORCE CORS HEADERS (CRITICAL FIX)
-  res.setHeader("Access-Control-Allow-Origin", "https://brainbeex.netlify.app");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
